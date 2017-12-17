3e0923bc80bb207aef7fda207807687a2286f5f3
Alan Leggitt <leggitta3@gmail.com>
2017-12-16 17:25:13 -0800

    camera centered on hero

--- js/main.js
+++ js/main.js
@@ -56,17 +56,22 @@ PlayState.init = function() {
 };
 
 PlayState.create = function() {
+    this.game.world.setBounds(-200, -200, 400, 400);
+    this.game.add.image(-200, -200, 'background')
     this._spawnCharacters();
 };
 
 // load game assets here
 PlayState.preload = function() {
+    this.game.load.image('background', 'images/background.png');
     this.game.load.spritesheet('hero', 'images/hero.png', 32, 32);
 };
 
 PlayState._spawnCharacters = function () {
     // spawn hero
     this.hero = new Hero(this.game, 0, 0);
+    this.hero.fixedToCamera = true;
+    this.hero.cameraOffset.setTo(150, 150);
     this.game.add.existing(this.hero);
 };
 
@@ -80,24 +85,36 @@ PlayState._handleCollisions = function() {
 
 PlayState._handleInput = function() {
     if (this.keys.left.isDown) {
-        this.hero.move(-1, 0);
+        //this.hero.move(-1, 0);
+        this.game.camera.x -= 4;
+        this.hero.angle = 270;
     }
     else if (this.keys.right.isDown) {
-        this.hero.move(1, 0);
+        //this.hero.move(1, 0);
+        this.game.camera.x += 4;
+        this.hero.angle = 90;
     }
     else if (this.keys.up.isDown) {
-        this.hero.move(0, -1);
+        //this.hero.move(0, -1);
+        this.game.camera.y -= 4;
+        this.hero.angle = 0;
     }
     else if (this.keys.down.isDown) {
-        this.hero.move(0, 1);
+        //this.hero.move(0, 1);
+        this.game.camera.y += 4;
+        this.hero.angle = 180;
     }
     else {
-        this.hero.move(0, 0)
+        //this.hero.move(0, 0)
     }
 };
 
+function render() {
+    this.game.debug.cameraInfo(this.game.camera, 32, 32);
+};
+
 window.onload = function () {
-    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    let game = new Phaser.Game(300, 300, Phaser.AUTO, 'game');
     game.state.add('play', PlayState);
     game.state.start('play');
 };
