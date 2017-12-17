68521323179debc95d121c902fb3072bcdd650d1
Alan Leggitt <leggitta3@gmail.com>
2017-12-16 17:49:54 -0800

    animate while walking. bind hero to background (hard coded solution)

--- js/main.js
+++ js/main.js
@@ -4,38 +4,20 @@ function Hero(game, x, y) {
     this.anchor.set(0.5, 0.5);
     this.game.physics.enable(this);
     this.body.collideWorldBounds = true;
-    this.animations.add('walk', [0, 1, 2, 1, 0], 8, false);
+    this.animations.add('walk', [0, 1], 8, false);
     this.animations.add('attack', [2, 3, 2], 8, false);
 }
 
 // inherit from Phaser.Sprite
 Hero.prototype = Object.create(Phaser.Sprite.prototype);
 Hero.prototype.constructor = Hero;
-Hero.prototype.move = function (x_dir, y_dir) {
-    const SPEED = 200;
-    this.body.velocity.x = x_dir * SPEED;
-    if (this.body.velocity.x < 0) {
-        this.angle = 270;
-        
-    } else if (this.body.velocity.x > 0) {
-        this.angle = 90;
-    }
-    this.body.velocity.y = y_dir * SPEED;
-    if (this.body.velocity.y < 0) {
-        this.angle = 0;
-    } else if (this.body.velocity.y > 0) {
-        this.angle = 180;
-    }
-};
-
+Hero.prototype.isMoving = false;
 Hero.prototype.attack = function () {
     this.animations.play('attack');
 };
 
 Hero.prototype.update = function() {
-    if (this.body.velocity.x !== 0) {
-        this.animations.play('walk');
-    } else if (this.body.velocity.y !== 0) {
+    if (this.isMoving) {
         this.animations.play('walk');
     }
 }
@@ -56,8 +38,8 @@ PlayState.init = function() {
 };
 
 PlayState.create = function() {
-    this.game.world.setBounds(-200, -200, 400, 400);
-    this.game.add.image(-200, -200, 'background')
+    this.game.world.setBounds(-150+16, -150+16, 900-32, 900-32);
+    this.game.add.image(0, 0, 'background')
     this._spawnCharacters();
 };
 
@@ -88,24 +70,29 @@ PlayState._handleInput = function() {
         //this.hero.move(-1, 0);
         this.game.camera.x -= 4;
         this.hero.angle = 270;
+        this.hero.isMoving = true;
     }
     else if (this.keys.right.isDown) {
         //this.hero.move(1, 0);
         this.game.camera.x += 4;
         this.hero.angle = 90;
+        this.hero.isMoving = true;
     }
     else if (this.keys.up.isDown) {
         //this.hero.move(0, -1);
         this.game.camera.y -= 4;
         this.hero.angle = 0;
+        this.hero.isMoving = true;
     }
     else if (this.keys.down.isDown) {
         //this.hero.move(0, 1);
         this.game.camera.y += 4;
         this.hero.angle = 180;
+        this.hero.isMoving = true;
     }
     else {
         //this.hero.move(0, 0)
+        this.hero.isMoving = false;
     }
 };
 
